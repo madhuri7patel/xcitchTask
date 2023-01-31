9031fa8c1af83ec26de2d3b40beeb7e8d9d68e72
madhuri7patel <madhuri7patel@gmail.com>
2023-02-01 01:05:28 +0530

    completed

--- fronted/products/src/App.js
+++ fronted/products/src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import Cart from "./Components/Cart";
+import Details from "./Components/Details";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Provider store={store}>
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" exact component={Cart} />
+        <Route path="/details/:id" exact component={Details} />
+      </Provider>
+    </Router>
+  );
 }
 
 export default App;
